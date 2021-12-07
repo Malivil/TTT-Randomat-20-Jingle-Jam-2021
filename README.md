e6de756436109d6bcf1bc66e8f8b175e3c64d29c
Malivil <mlebson@outlook.com>
2021-12-07 17:51:16 -0500

    Added new Jingle Bells event

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# TTT-Randomat-20-Jingle-Jam-2021
+# Randomat 2.0 Events Pack for Jingle Jam 2021
+A pack of [Randomat 2.0](https://github.com/Malivil/TTT-Randomat-20) events created based on the generous donations of our community members in support of [Jingle Jam 2021](https://www.jinglejam.co.uk/).
+
+# Events
+
+## Jingle Bells
+_Suggested By_: Noxx\
+All players play the Loot Goblin jingle noise when they move
+\
+\
+**ConVars**
+\
+_ttt_randomat_jinglebells - Default: 1 - Whether this event is enabled.\
+_ttt_randomat_jinglebells_min_players_ - Default: 0 - The minimum number of players required for this event to start.\
+_ttt_randomat_jinglebells_weight_ - Default: -1 - The weight this event should use during the randomized event selection process.
---
+++ lua/randomat2/events/jinglebells.lua
@@ -0,0 +1,29 @@
+local EVENT = {}
+
+EVENT.Title = "Jingle Bells"
+EVENT.Description = "Jingle all the way!"
+EVENT.id = "jinglebells"
+
+local footsteps = {
+    Sound("lootgoblin/jingle1.wav"),
+    Sound("lootgoblin/jingle2.wav"),
+    Sound("lootgoblin/jingle3.wav"),
+    Sound("lootgoblin/jingle4.wav"),
+    Sound("lootgoblin/jingle5.wav"),
+    Sound("lootgoblin/jingle6.wav"),
+    Sound("lootgoblin/jingle7.wav"),
+    Sound("lootgoblin/jingle8.wav")
+}
+function EVENT:Begin()
+    self:AddHook("PlayerFootstep", function(ply, pos, foot, snd, volume, rf)
+        local idx = math.random(1, #footsteps)
+        local chosen_sound = footsteps[idx]
+        sound.Play(chosen_sound, pos, volume, 100, 1)
+    end)
+end
+
+function EVENT:Condition()
+    return ROLE_LOOTGOBLIN and ROLE_LOOTGOBLIN > ROLE_NONE
+end
+
+Randomat:register(EVENT)
