084ae2b7a35dd505b786610b7c3c841b34f83c4a
Malivil <mlebson@outlook.com>
2022-02-26 18:12:01 -0500

    Added notification in the chat window telling a player what gift they got from santa, or what is in the present if they can't pick it up

--- gamemodes/terrortown/entities/entities/randomat_hellosanta_present/shared.lua
+++ gamemodes/terrortown/entities/entities/randomat_hellosanta_present/shared.lua
@@ -15,6 +15,16 @@ function ENT:Initialize()
 end
 
 if SERVER then
+    util.AddNetworkString("TTT_RdmtSantaPresentNotify")
+
+    local function NotifyPlayer(ply, item, has, can_carry)
+        net.Start("TTT_RdmtSantaPresentNotify")
+        net.WriteString(tostring(item))
+        net.WriteBool(has)
+        net.WriteBool(can_carry)
+        net.Send(ply)
+    end
+
     function ENT:Use(activator)
         if CurTime() > self.nextUse then
             if not IsValid(activator) or not activator:Alive() or activator:IsSpec() then return end
@@ -23,19 +33,15 @@ if SERVER then
             local owner = self:GetOwner()
             if activator:RdmtCheckSantaGift(owner) then
                 local item_id = self.item_id
-
-                if activator:CanCarryWeapon(weapons.GetStored(item_id)) then
-                    if activator:HasWeapon(item_id) then
-                        activator:PrintMessage(HUD_PRINTTALK, "You already have this item!")
-                        activator:RdmtUndoSantaGift(owner)
-                        return
-                    else
-                        activator:Give(item_id)
-                    end
-                else
-                    activator:PrintMessage(HUD_PRINTTALK, "You are already holding an item that shares a slot with this gift!")
+                local has = activator:HasWeapon(item_id)
+                local can_carry = activator:CanCarryWeapon(weapons.GetStored(item_id))
+                NotifyPlayer(activator, item_id, has, can_carry)
+                if has or not can_carry then
                     activator:RdmtUndoSantaGift(owner)
                     return
+                else
+                    activator:Give(item_id)
+                    Randomat:CallShopHooks(nil, item_id, activator)
                 end
 
                 owner:PrintMessage(HUD_PRINTTALK, activator:Nick() .. " has opened your present and your ammo has been refunded.")
@@ -44,4 +50,24 @@ if SERVER then
             end
         end
     end
+end
+
+if CLIENT then
+    net.Receive("TTT_RdmtSantaPresentNotify", function()
+        local client = LocalPlayer()
+        if not IsPlayer(client) then return end
+
+        local item = net.ReadString()
+        local has = net.ReadBool()
+        local can_carry = net.ReadBool()
+        local name = Randomat:GetWeaponName(item)
+
+        if has then
+            client:PrintMessage(HUD_PRINTTALK, "You already have '" .. name .. "'!")
+        elseif not can_carry then
+            client:PrintMessage(HUD_PRINTTALK, "You are already holding an item that shares a slot with '" .. name .. "'!")
+        else
+            client:PrintMessage(HUD_PRINTTALK, "You got '" .. name .. "' from " .. ROLE_STRINGS[ROLE_SANTA] .. "!")
+        end
+    end)
 end
