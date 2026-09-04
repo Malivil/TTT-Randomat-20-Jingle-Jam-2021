59a475aaf5e4c04c5771a5a68a2d7e44d00d7f71
Malivil <mlebson@outlook.com>
2026-09-04 11:17:39 -0400

    Fixed minor comment typo

--- lua/randomat2/events/boxingday.lua
+++ lua/randomat2/events/boxingday.lua
@@ -240,7 +240,7 @@ function plymeta:RdmtBoxingKnockout()
     self:Spectate(OBS_MODE_CHASE)
     self:SpectateEntity(ragdoll)
 
-    -- The diguiser stays in their hand so hide it from view
+    -- The disguiser stays in their hand so hide it from view
     self:DrawViewModel(false)
     self:DrawWorldModel(false)
 
